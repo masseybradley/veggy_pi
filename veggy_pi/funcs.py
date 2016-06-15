d9a9f1521ebd54b81e37fd188e1cfcf35fff58a5
Bradley Massey <bmassey@enbesup10.eastnets>
2016-06-15 11:08:39 +0200

    removed static functions from models - added in funcs

--- veggy_pi/funcs.py
+++ veggy_pi/funcs.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from django.utils.translation import ugettext_lazy as _
+
 
 def is_number(num):
     """
@@ -18,3 +20,15 @@ def all_numbers(*numbers):
         if not is_number(num):
             return False
     return True
+
+
+def is_greater_than(max_val, min_val):
+    if min_val > max_val or min_val == max_val:
+        raise ValueError(_('%s must not be greater than %s' % (min_val, max_val))) 
+        
+
+def value_in_range(val, max_range):
+    if val not in max_range:
+        raise ValueError(_('%s must be in range %s' % (val, max_range)))
+
+
